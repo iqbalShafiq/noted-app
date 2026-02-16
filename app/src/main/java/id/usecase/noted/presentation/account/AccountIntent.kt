8806d2a323e06c18847f46aaf797c954cd436fbf
syafiq <iqbal.syafiq.rozaan@gmail.com>
2026-02-16 17:43:54 +0700

    Task 15: Add RefreshProfile intent to AccountIntent

--- app/src/main/java/id/usecase/noted/presentation/account/AccountIntent.kt
+++ app/src/main/java/id/usecase/noted/presentation/account/AccountIntent.kt
@@ -3,6 +3,8 @@ package id.usecase.noted.presentation.account
 sealed interface AccountIntent {
     data object LoadAccountInfo : AccountIntent
 
+    data object RefreshProfile : AccountIntent
+
     data object LoginClicked : AccountIntent
 
     data object LogoutClicked : AccountIntent
